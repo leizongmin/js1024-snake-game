b0032441bc0b24638c967fa8ced0af99d80af497
Zongmin Lei <leizongmin@gmail.com>
2021-07-10 12:43:13 +0800

    finish mini version in 1021 bytes

--- game.js
+++ game.js
@@ -1,10 +1,9 @@
 (() => {
-  const pointWidth = 30;
-  const pointHeight = 30;
-  const screenWidth = ~~(a.width / pointWidth) * pointWidth;
-  const screenHeight = ~~(a.height / pointHeight) * pointHeight;
-  const horizontalSize = ~~(screenWidth / pointWidth);
-  const verticalSize = ~~(screenHeight / pointHeight);
+  const pointSize = 30;
+  const screenWidth = ~~(a.width / pointSize) * pointSize;
+  const screenHeight = ~~(a.height / pointSize) * pointSize;
+  const horizontalSize = ~~(screenWidth / pointSize);
+  const verticalSize = ~~(screenHeight / pointSize);
   const keyCodeMap = {
     97: { x: -1, y: 0, o: 100 }, // key [A] => [Left]
     115: { x: 0, y: 1, o: 119 }, // key [S] => [Down]
@@ -23,7 +22,7 @@
     const key = keyCodeMap[e.which];
     if (key) {
       const head = body[0];
-      if (body.length > 1 && key.o === head.d) {
+      if (body.length > 1 && key.o == head.d) {
         // no backsliding allowed
         return;
       }
@@ -32,9 +31,9 @@
   });
   function drawPoint(p, color) {
     c.strokeStyle = "#fff";
-    c.strokeRect(p.x * pointWidth, p.y * pointHeight, pointWidth, pointHeight);
+    c.strokeRect(p.x * pointSize, p.y * pointSize, pointSize, pointSize);
     c.fillStyle = color || "#aaa";
-    c.fillRect(p.x * pointWidth, p.y * pointHeight, pointWidth, pointHeight);
+    c.fillRect(p.x * pointSize, p.y * pointSize, pointSize, pointSize);
   }
   function randomFood() {
     const p = {
@@ -42,7 +41,7 @@
       y: ~~(Math.random() * verticalSize),
     };
     // if it overlaps with the body, try the make a new point
-    return body.some((p2) => p.x === p2.x && p.y === p2.y) ? randomFood() : p;
+    return body.some((p2) => p.x == p2.x && p.y == p2.y) ? randomFood() : p;
   }
   function start() {
     body = [{ x: ~~(horizontalSize / 2), y: ~~(verticalSize / 2), d: 119 }]; // start at the screen midpoint, upward
@@ -50,7 +49,7 @@
     loop(); // enter game loop
   }
   function gameOver() {
-    c.fillStyle = "#e96900";
+    c.fillStyle = "#e60";
     c.font = "bold 48px serif";
     c.fillText(
       "Game Over",
@@ -81,17 +80,17 @@
       return gameOver();
     }
     body.unshift(head); // 没有吃到食物，蛇头前进时，蛇尾要收缩
-    if (food.x === head.x && food.y === head.y) {
+    if (food.x == head.x && food.y == head.y) {
       // if it's already eaten the food, then make next food
       food = randomFood();
     } else {
       // or else, move the body forward
       body.pop();
     }
-    c.fillStyle = "#f1fedd";
+    c.fillStyle = "#eee";
     c.fillRect(0, 0, screenWidth, screenHeight);
-    drawPoint(food, "#e96900"); // draw food
-    drawPoint(body[0], "#2d64b3"); // draw snake's head
+    drawPoint(food, "#e60"); // draw food
+    drawPoint(body[0], "#429"); // draw snake's head
     body.slice(1).forEach((p) => drawPoint(p)); // draw snake's body
     tid = setTimeout(loop, 150 - body.length * 1.5); // compute speed
   }
