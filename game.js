cda68441bc4aedbac2e89d5735da397f78a478c7
Zongmin Lei <leizongmin@gmail.com>
2021-07-10 11:53:31 +0800

    es6 style

--- game.js
+++ game.js
@@ -1,40 +1,41 @@
 (() => {
-  var w = 20;
-  var h = 20;
-  var pw = a.width / w;
-  var ph = a.height / h;
-  var keyCode = {
+  const horizontalSize = 20;
+  const verticalSize = 20;
+  const pointWidth = a.width / horizontalSize;
+  const pointHeight = a.height / verticalSize;
+  const keyCodeMap = {
     97: { x: -1, y: 0 },
     115: { x: 0, y: 1 },
     100: { x: 1, y: 0 },
     119: { x: 0, y: -1 },
   }; // A:97, S:115, D:100, W:119
-  var body = [];
-  var food = randomFood();
+  let body = [];
+  let food = randomFood();
+  let tid; // timer id, used to pause or continue the game
   addEventListener("keypress", (e) => {
     if (e.which == 32) {
       tid ? (clearTimeout(tid), (tid = null)) : loop();
     }
-    if (keyCode[e.which]) {
+    if (keyCodeMap[e.which]) {
       body[0].d = e.which;
     }
   });
   function drawPoint(p, color) {
     c.strokeStyle = "#fff";
-    c.strokeRect(p.x * pw, p.y * ph, pw, ph);
+    c.strokeRect(p.x * pointWidth, p.y * pointHeight, pointWidth, pointHeight);
     c.fillStyle = color || "#aaa";
-    c.fillRect(p.x * pw, p.y * ph, pw, ph);
+    c.fillRect(p.x * pointWidth, p.y * pointHeight, pointWidth, pointHeight);
   }
   function randomFood() {
-    var p = {
-      x: ~~(Math.random() * w),
-      y: ~~(Math.random() * h),
+    const p = {
+      x: ~~(Math.random() * horizontalSize),
+      y: ~~(Math.random() * verticalSize),
     };
     // if it overlaps with the body, try the make a new point
     return body.some((p2) => p.x === p2.x && p.y === p2.y) ? randomFood() : p;
   }
   function start() {
-    body = [{ x: ~~(w / 2), y: ~~(h / 2), d: 119 }]; // start at the screen midpoint, upward
+    body = [{ x: ~~(horizontalSize / 2), y: ~~(verticalSize / 2), d: 119 }]; // start at the screen midpoint, upward
     loop(); // enter game loop
   }
   function gameOver() {
@@ -43,17 +44,24 @@
     c.fillText("Game Over", (a.width - 24 * 4) / 2, (a.height - 24) / 2);
     setTimeout(start, 2000);
   }
-  function nextPosition(p) {
-    var d = keyCode[p.d];
-    return { x: p.x + d.x, y: p.y + d.y, d: p.d };
+  function nextPosition(point) {
+    const direction = keyCodeMap[point.d];
+    return { x: point.x + direction.x, y: point.y + direction.y, d: point.d };
   }
   function loop() {
-    var head = nextPosition(body[0]); // get the next position
+    const head = nextPosition(body[0]); // get the next position
     // if it's out of bounds, then game over
-    if (!(head.x >= 0 && head.x < w && head.y >= 0 && head.y < h)) {
+    if (
+      !(
+        head.x >= 0 &&
+        head.x < horizontalSize &&
+        head.y >= 0 &&
+        head.y < verticalSize
+      )
+    ) {
       return gameOver();
     }
-    // for (var i = 0; i < pb.length; i++) {
+    // for (let i = 0; i < pb.length; i++) {
     //   if (head.x === pb[i].x && head.y === pb[i].y) {
     //     return gameOver();
     //   }
@@ -73,6 +81,5 @@
     body.slice(1).forEach((p) => drawPoint(p)); // draw snake's body
     tid = setTimeout(loop, 200 - body.length * 1.5); // compute speed
   }
-  var tid;
   start();
 })();
