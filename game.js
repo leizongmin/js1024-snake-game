6ceb028bba900c34a02b447a8e883d37b95d2ff0
Zongmin Lei <leizongmin@gmail.com>
2021-07-10 11:46:36 +0800

    add readable comment

--- game.js
+++ game.js
@@ -1,42 +1,41 @@
 (() => {
-  var w = 20,
-    h = 20,
-    pw = a.width / w,
-    ph = a.height / h;
+  var w = 20;
+  var h = 20;
+  var pw = a.width / w;
+  var ph = a.height / h;
   var keyCode = {
     97: { x: -1, y: 0 },
     115: { x: 0, y: 1 },
     100: { x: 1, y: 0 },
     119: { x: 0, y: -1 },
   }; // A:97, S:115, D:100, W:119
-  var pb = [],
-    pf = rndp();
+  var body = [];
+  var food = randomFood();
   addEventListener("keypress", (e) => {
     if (e.which == 32) {
       tid ? (clearTimeout(tid), (tid = null)) : loop();
     }
-    if (keyCode[e.which]) pb[0].d = e.which;
+    if (keyCode[e.which]) {
+      body[0].d = e.which;
+    }
   });
-  function drawPoint(p, a, b, color) {
+  function drawPoint(p, color) {
     c.strokeStyle = "#fff";
     c.strokeRect(p.x * pw, p.y * ph, pw, ph);
     c.fillStyle = color || "#aaa";
     c.fillRect(p.x * pw, p.y * ph, pw, ph);
   }
-  function rndp() {
+  function randomFood() {
     var p = {
       x: ~~(Math.random() * w),
       y: ~~(Math.random() * h),
     };
-    return pb.some(function (p2) {
-      return p.x === p2.x && p.y === p2.y;
-    })
-      ? rndp()
-      : p;
+    // if it overlaps with the body, try the make a new point
+    return body.some((p2) => p.x === p2.x && p.y === p2.y) ? randomFood() : p;
   }
   function start() {
-    pb = [{ x: ~~(w / 2), y: ~~(h / 2), d: 119 }];
-    loop();
+    body = [{ x: ~~(w / 2), y: ~~(h / 2), d: 119 }]; // start at the screen midpoint, upward
+    loop(); // enter game loop
   }
   function gameOver() {
     c.fillStyle = "#e96900";
@@ -44,24 +43,36 @@
     c.fillText("Game Over", (a.width - 24 * 4) / 2, (a.height - 24) / 2);
     setTimeout(start, 2000);
   }
-  function nxp(p) {
+  function nextPosition(p) {
     var d = keyCode[p.d];
     return { x: p.x + d.x, y: p.y + d.y, d: p.d };
   }
   function loop() {
-    var hp = nxp(pb[0]); // 计算前进后蛇头的位置，如果不在屏幕区域内，或者与蛇身重合，则游戏结束
-    if (!(hp.x >= 0 && hp.x < w && hp.y >= 0 && hp.y < h)) return gameOver();
-    for (var i = 0; i < pb.length; i++)
-      if (hp.x === pb[i].x && hp.y === pb[i].y) return gameOver();
-    pb.unshift(hp); // 没有吃到食物，蛇头前进时，蛇尾要收缩
-    pf.x === hp.x && pf.y === hp.y ? (pf = rndp()) : pb.pop();
+    var head = nextPosition(body[0]); // get the next position
+    // if it's out of bounds, then game over
+    if (!(head.x >= 0 && head.x < w && head.y >= 0 && head.y < h)) {
+      return gameOver();
+    }
+    // for (var i = 0; i < pb.length; i++) {
+    //   if (head.x === pb[i].x && head.y === pb[i].y) {
+    //     return gameOver();
+    //   }
+    // }
+    body.unshift(head); // 没有吃到食物，蛇头前进时，蛇尾要收缩
+    if (food.x === head.x && food.y === head.y) {
+      // if it's already eaten the food, then make next food
+      food = randomFood();
+    } else {
+      // or else, move the body forward
+      body.pop();
+    }
     c.fillStyle = "#f1fedd";
     c.fillRect(0, 0, a.width, a.height);
-    drawPoint(pf, 0, 0, "#e96900"); // Food
-    drawPoint(pb[0], 0, 0, "#2d64b3"); // snake's head
-    pb.slice(1).forEach(drawPoint); // snake's body
-    tid = setTimeout(loop, 200 - pb.length * 1.5); // speed
+    drawPoint(food, "#e96900"); // draw food
+    drawPoint(body[0], "#2d64b3"); // draw snake's head
+    body.slice(1).forEach((p) => drawPoint(p)); // draw snake's body
+    tid = setTimeout(loop, 200 - body.length * 1.5); // compute speed
   }
-  start();
   var tid;
+  start();
 })();
